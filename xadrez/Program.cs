0e205381360b0c9227746e3e123adb2927c43ab0
Julia Karolina <juliakarolina24@gmail.com>
2021-07-06 13:51:36 -0300

    Implementando a logic de xequemate

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -42,7 +42,8 @@ namespace xadrez
                         Console.ReadLine();
                     }
                 }
-
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch(TabuleiroException e)
             {
